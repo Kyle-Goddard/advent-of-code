ce6be337635ae841ba420ab136084ac6174d8553
Kyle Goddard <kyle.goddard@saltpay.co>
2022-12-19 08:21:20 +0200

    day 10 part II complete

--- 2022/day_10_cathode_ray_tube/solution.py
+++ 2022/day_10_cathode_ray_tube/solution.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 with open(
     "/Users/kyle/Documents/Learning/advent-of-code/2022/day_10_cathode_ray_tube/puzzle_input.txt",
     "r",
@@ -25,10 +27,28 @@ def after_cycle(id, register):
     return register[id]
 
 
-cycles = [20, 60, 100, 140, 180, 220]
-sum_of_cycles = 0
+# cycles = [20, 60, 100, 140, 180, 220]
+# sum_of_cycles = 0
 
-for c in cycles:
-    sum_of_cycles += c * during_cycle(c, register)
+# for c in cycles:
+#     sum_of_cycles += c * during_cycle(c, register)
 
-print(sum_of_cycles)
+# print(sum_of_cycles)
+
+screen = np.array([[" "] * 40] * 6)
+
+for cycle in range(40 * 6):
+    col = cycle % 40
+    row = cycle // 40
+
+    sprite_pos = after_cycle(cycle, register) - 1
+
+    region = list(range(sprite_pos, sprite_pos + 3))
+
+    if col in region:
+        screen[row][col] = "#"
+    else:
+        continue
+
+for i in range(6):
+    print("".join(screen[i]))
