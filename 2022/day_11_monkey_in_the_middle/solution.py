2a801bf69f6baca1b828c1ac4dd226ed27048d6f
Kyle Goddard <kyle.goddard@saltpay.co>
2022-12-25 10:03:33 +0200

    day 11 incomplete

---
+++ 2022/day_11_monkey_in_the_middle/solution.py
@@ -0,0 +1,23 @@
+class Monkey:
+    def __init__(
+        self,
+        items=[],
+    ) -> None:
+        self.items = items
+
+
+def parse(lines):
+    monkeys = []
+    # for i, line in enumerate(lines):
+
+    return monkeys
+
+
+with open(
+    "/Users/kyle/Documents/Learning/advent-of-code/2022/day_11_monkey_in_the_middle/input.txt",
+    "r",
+) as file:
+    lines = file.readlines()
+
+print(lines)
+monkeys = parse(lines)
